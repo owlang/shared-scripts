01e56b8533a7116579af57404a6b391bdae2a1b8
Olivia Wen-Mei Lang <oliviawlang@gmail.com>
2022-09-08 15:48:14 -0400

    major update/fix FragMap
    The following changes have been tentatively implemented. Heatmaps are still looking suspicious: possible anti/sense mixup somewhere.
    - move script info from comments at the top to argparse help message
    - switch to use Numpy matrices for storing FragMap values
    - separate out strand-specific fragmaps
    - add support for Read2 5' end
    - add support for strand-specific reference coord pileups
    - add support for fragment size binning
    - add test commands for fragment binning
    - fix typing of argument parsing
    - add initialized input var values print statements
    - add mapping/secondary/supplemental/duplicate filters

--- fragmap.py
+++ fragmap.py
@@ -1,13 +1,10 @@
 import sys,argparse
 import pysam
+import numpy as np
 
-# Mimic David Price's FragMap figures from CSHL 2021
+# Based on David Price's introduced FragMap figure idea from CSHL 2021.
 
 # At some point this should be compared to the Price lab version when they fill in the as-of-now empty repostitory: https://github.com/P-TEFb/fragMap
-
-# Basically a tab pileup by fragment size instead of gene-wise...smooshing figure down to x-axis would result in a composite plot and smooshing it left to the y-axis would result in fragment size histogram plot
-
-# Right now it is written to output CDT files that are compatible with ScriptManager for visualization
 
 # Send this to Jordan when done so that he can visualize BZ data with this
 
@@ -20,7 +17,7 @@ def validateBED(bedfilename):
 		if(line.find("#")==0):
 			continue
 		tokens = line.strip().split("\t")
-		chr,start,stop = tokens[0],int(tokens[1]),int(tokens[2])
+		chr,start,stop,strand = tokens[0],int(tokens[1]),int(tokens[2]),tokens[5]
 		# Check start/stop validity
 		if(start>stop):
 			raise RuntimeError("Invalid BED coordinates: %s\n" % (line.strip()))
@@ -32,7 +29,7 @@ def validateBED(bedfilename):
 		if(interval_size!=(stop-start)):
 			raise RuntimeError("BED interval size does not match the others: %s\n" % (line.strip()))
 			return(False)
-		COORDS.append((chr,start,stop))
+		COORDS.append((chr,start,stop,strand))
 	reader.close()
 	if(len(COORDS)<1):
 		sys.stderr.write("BED File must contain at least one coordinate!\n")
@@ -43,6 +40,20 @@ def bin_fragments(matrix,fbin=1):
 	if(fbin==1):
 		return(matrix)
 	print("Bin matrix into appropriate fragment bins")
+	# print(matrix)
+	BINNED_MATRIX = np.zeros((1,np.shape(matrix)[1]))
+	index = 0
+	for i in range(np.shape(matrix)[0]):
+		# print("(index,i): (%i, %i)" % (index, i))
+		if( i % fbin==0 and i!=0):
+			BINNED_MATRIX = np.vstack((BINNED_MATRIX, matrix[i,:]))
+			index += 1
+		else:
+			BINNED_MATRIX[index,:] = np.add(BINNED_MATRIX[index,:], matrix[i,:])
+		# print(BINNED_MATRIX)
+	# print("FINAL...")
+	# print(BINNED_MATRIX)
+	return(BINNED_MATRIX)
 
 def bin_genomic(matrix,gbin=1):
 	if(gbin==1):
@@ -51,7 +62,15 @@ def bin_genomic(matrix,gbin=1):
 
 def getParams():
 	'''Parse parameters from the command line'''
-	parser = argparse.ArgumentParser(description='Attempt to recreate FragMap figure from David Price lab (code not yet available) for visualization and eventual incorporation into ScriptManager.')
+	parser = argparse.ArgumentParser(description= \
+	'''Attempt to recreate FragMap figure from David Price lab (code not yet available) for visualization and eventual incorporation into ScriptManager.
+
+	Basically a tab pileup by fragment size instead of gene-wise...
+		- smooshing figure down to x-axis would result in a composite plot and
+		- smooshing it left to the y-axis would result in fragment size histogram plot
+
+	Right now it is written to output CDT files that are compatible with ScriptManager for visualization
+	''')
 
 	# parser.add_argument('-d','--data-file', metavar='data_file', dest='data_file', required=True, help='')
 	parser.add_argument('-i','--bed-intervals', metavar='bedfile', dest='bedfilepath', required=True, help='The BED intervals to pileup on for FragMap figure')
@@ -60,9 +79,9 @@ def getParams():
 	# Set minimum fragment size (bp) = 0
 	# Set maximum fragment size (bp) = 400
 	# Set fragment bin size (bp) = 1
-	parser.add_argument('--fmin', metavar='minFragSize', dest='fmin', required=False, default=0, help='The minimum fragment size to track (bp)')
-	parser.add_argument('--fmax', metavar='maxFragSize', dest='fmax', required=False, default=400, help='The maximum fragment size to track (bp)')
-	parser.add_argument('--fbin', metavar='binFragSize', dest='fbin', required=False, default=1, help='The fragment size bin size(bp)')
+	parser.add_argument('--fmin', metavar='minFragSize', dest='fmin', type=int, required=False, default=0, help='The minimum fragment size to track (bp)')
+	parser.add_argument('--fmax', metavar='maxFragSize', dest='fmax', type=int, required=False, default=400, help='The maximum fragment size to track (bp)')
+	parser.add_argument('--fbin', metavar='binFragSize', dest='fbin', type=int, required=False, default=1, help='The fragment size bin size(bp)')
 
 	# Set genomic bin size (bp) = 1
 	parser.add_argument('--gbin', metavar='binIntervalSize', dest='gbin', required=False, default=1, help='The genomic size bin size (bp)')
@@ -70,104 +89,167 @@ def getParams():
 	# Sum vs norm per bed interval = sum total
 
 	# separate vs combined = combined
+	parser.add_argument('--separate', action='store_true', dest='separate', help="Store FragMap matrices in a strand-specific manner")
 
-	# Set read = Read1
-	# read1, read2, all, midpoint
+	# Set read = 1
+	# fragment, read1, read2, midpoint
+	parser.add_argument('--read', metavar='readEncoding', dest='read', type=int, required=False, default=1, help= \
+	'''The positional encoding along the sequenced fragment to use:
+		0 = full fragment length;
+		1 = 5\' end of read 1 (default);
+		2 = 5\' end of read 2;
+		3 = fragment midpoint;
+	''')
 
 	# Require PE = True
-	parser.add_argument('-p','--require-pe', dest='requirePE', required=False, default=True, help='The fragments included in the count must have both reads mapped')
+	parser.add_argument('-p','--require-pe', dest='requirePE', action='store_true', help='The fragments included in the count must have both reads mapped')
 
-	# Set output composite file = composite_average.out
 	# Set output CDT file basename = FragMap_<BAM>_<BED>_<read>_<sep>
-	parser.add_argument('-o','--output-cdt', metavar='outfile', dest='outfilepath', required=True, help='The CDT output filepath to save FragMap matrix to')
-
+	parser.add_argument('-o','--output-cdt', metavar='outfile', dest='outfilepath', required=True, help='The CDT output basename to save FragMap matrix/ces to')
 
 	args = parser.parse_args()
+
+	# Re-adjust requirePE arg according to read encoding
+	if(args.read in [0,3]):
+		args.requirePE = true
+		print("Non-Read1/2 5' end pileups not supported yet.")
+		exit()
+
 	return(args)
 
+def unit_tests():
+	# TEST: Fragment Binning from test matrix
+	bin_fragments(np.arange(30).reshape(10,3),4)
 
 if __name__ == '__main__':
 	'''Main pileup function'''
+
 	args = getParams()
+
 	COORDS = validateBED(args.bedfilepath)
-	FRAGMAP = None
-
 	GLEN = COORDS[0][2]-COORDS[0][1]
-	FLEN = args.fmax-args.fmin
+	FLEN = args.fmax-args.fmin +1
+	print("COORDS: %i coordinates loaded from %s" % (len(COORDS),args.bedfilepath))
+	print("GLEN(bp): %i and FLEN(bp): %i" % (GLEN,FLEN))
+	print("Fragment Limits(bp): %i to %i" % (args.fmin,args.fmax))
+	print("Fragment Bins(bp): %i" % (args.fbin))
+	print("Genomic Bins(bp): %i" % (args.gbin))
+	print("Strand Separated: %r" % (args.separate))
+	print("Outpath basename: %s" % (args.outfilepath))
 
+	# Initialize separate matrices for same and opposite strand
+	MATRIX_SAME = np.zeros((FLEN, GLEN))
+	MATRIX_OPPOSITE = None
+	if(args.separate):
+		MATRIX_OPPOSITE = np.zeros((FLEN, GLEN))
+	sys.stderr.write("Local matrix dim: (%i,%i)\n" % (len(MATRIX_SAME),len(MATRIX_SAME[0])))
+
+	# print("x: %i" % len(MATRIX_SAME))
+	# print("y: %i" % len(MATRIX_SAME[0]))
 	# open BAM file
 	samfile = pysam.AlignmentFile(args.bamfilepath, "rb")
 	# parse BED coords
 	for coord in COORDS:
-		MATRIX = [[0.]*GLEN] * FLEN
-		sys.stderr.write("Local matrix dim: (%i,%i)\n" % (len(MATRIX),len(MATRIX[0])))
-		TOTAL_TAGS = 0
+		COORD_TAGS = 0
 
 		# parse overlapping BAM reads
 		print(coord)
 		for read in samfile.fetch(coord[0],coord[1]-400,coord[2]+400):
 			## Include read filter criteria
 			# Filter out unmapped or non-primary alignments
-			if(read.is_duplicate):
-				print("some read is duplicate")
+			if(read.is_unmapped):
+				print("thisRead is unmapped")
 				continue
-			#if(read.is_unmapped or read.is_secondary or read.is_supplementary or read.is_duplicate):
-			#	print("some read is unmapped/secondary/supplementary/duplicate")
-			#	continue
+			if(read.is_secondary):
+				print("thisRead is secondary")
+				continue
+			if(read.is_supplementary):
+				print("thisRead is secondary")
+				continue
+			if(read.is_duplicate):
+				print("thisRead is a duplicate")
+				continue
 			# Filter for proper pairs if requiring paired-end data
 			if(args.requirePE and (not read.is_paired)):
-				#print("some read is not pair")
+				print("thisRead is not pair")
 				# proper?
 				continue
-			# Filter to include only read1 (may change depending on args criteria)
-			#if(not read.is_read1):
-				#print("some read is not read1")
-			#	continue
-			# Later add read.is_reverse for separate flag
+			# Filter to include only read1/read2 according to encoding
+			if(args.read==0 or args.read==1 or args.read==3):
+				if(read.is_read2):
+					print("thisRead != R1 when required R1/M/F encoding")
+					continue
+			elif(args.read == 2):
+				if(read.is_read1):
+					print("thisRead != R2 when required R2 encoding")
+					continue
 
-			print(read.template_length)
-			print(read.cigarstring)
-			#print(read.cigarstring)
+			# Determine if read is same or opposite stranded
+			SAME = read.is_reverse == (coord[3]=="-")
+
+			# Select Read1 5' end position
 			index = read.reference_start - coord[1]
-			#print(coord[1])
-			#print(read.reference_start)
-			print(index)
-			# Skip reads whose point falls off the interval
+			if((read.is_reverse and args.read==1) or (not read.is_reverse and args.read==2)):
+				index = read.reference_end - coord[1] - 1
+			# if(coord[3]=="-"):
+			# 	index = GLEN - index
+
+			# Skip reads whose mark falls off the interval
 			if(index<0 or index>=GLEN):
-				print("skip this read")
+				print("thisRead falls outside coord interval")
 				continue
-			MATRIX[read.template_length][index] += 1
-			TOTAL_TAGS += 1
-		print(TOTAL_TAGS)
+			# Skip reads whose insert size falls outside captured interval
+			if(read.template_length<args.fmin or read.template_length>args.fmax):
+				print("fragment size falls outside coord interval")
+				continue
+
+			# Update appropriate MATRIX variable
+			if(not SAME and args.separate):
+				MATRIX_OPPOSITE[read.template_length-args.fmin][index] += 1
+			else:
+				# sys.stderr.write(str(read.template_length-args.fmin) + "\n")
+				MATRIX_SAME[read.template_length-args.fmin][index] += 1
+			COORD_TAGS += 1
+		print(COORD_TAGS)
 
 		# Total tag normalize the matrix for this BED Coordinate
 		# if(args.totaltag):
 		# MATRIX = [ [ i/TOTAL_TAGS for i in row] for row in MATRIX]
-		if(FRAGMAP==None):
-			FRAGMAP = MATRIX
-			continue
-		for i in range(FLEN):
-			for j in range(GLEN):
-				FRAGMAP[i][j] = FRAGMAP[i][j] + MATRIX[i][j]
+		# for i in range(FLEN):
+		# 	for j in range(GLEN):
+		# 		MATRIX_SAME[i][j] = FRAGMAP[i][j] + MATRIX[i][j]
 		#FRAGMAP = [ [ FRAGMAP[i][j]+MATRIX[i][j] for j in range(len(MATRIX[i]))] for i in range(len(MATRIX))]
-		print(MATRIX[0][120:130])
-		print(MATRIX[50][120:130])
 	# Close files
 	samfile.close()
 
 	# Reduce by genomic bin size
 	#FRAGMAP = bin_genomic(FRAGMAP,args.gbin)
 	# Reduce by fragment bin size
-	#FRAGMAP = bin_fragments(FRAGMAP,args.fbin)
-	print(MATRIX[0][120:130])
-	print(MATRIX[50][120:130])
+	MATRIX_SAME = bin_fragments(MATRIX_SAME,args.fbin)
+	if(args.separate):
+		MATRIX_OPPOSITE = bin_fragments(MATRIX_OPPOSITE,args.fbin)
 
-	# Output CDT
-	writer = open(args.outfilepath, 'w')
+
+	# Output CDT (sense)
+	OUTFILE_SAME = args.outfilepath + "_sense.cdt"
+	if(args.read==0 or args.read==3):
+		OUTFILE_SAME = args.outfilepath + "_combined.cdt"
+	writer = open(OUTFILE_SAME, 'w')
 	# print Genomic bin header
-	writer.write("\t" + "\t".join(["\t" + str(g) for g in range(len(FRAGMAP[0])) ]) + "\n" )
+	writer.write("\t" + "\t".join(["\t" + str(g) for g in range(len(MATRIX_SAME[0])) ]) + "\n" )
 	nextf = args.fmin
-	for f in range(len(FRAGMAP)):
-		writer.write("%i\t%i\t%s\n" % (nextf, nextf+args.fbin-1, "\t".join([ str(m) for m in FRAGMAP[f] ]) ))
+	for f in range(np.shape(MATRIX_SAME)[0]):
+		writer.write("%i\t%i\t%s\n" % (nextf, nextf+args.fbin-1, "\t".join([ str(MATRIX_SAME[f][m]) for m in range(np.shape(MATRIX_SAME)[1]) ]) ))
 		nextf = nextf+args.fbin
 	writer.close()
+
+	# Output CDT (anti)
+	if(args.separate):
+		writer = open(args.outfilepath + "_anti.cdt", 'w')
+		# print Genomic bin header
+		writer.write("\t" + "\t".join(["\t" + str(g) for g in range(len(MATRIX_OPPOSITE[0])) ]) + "\n" )
+		nextf = args.fmin
+		for f in range(np.shape(MATRIX_OPPOSITE)[0]):
+			writer.write("%i\t%i\t%s\n" % (nextf, nextf+args.fbin-1, "\t".join([ str(MATRIX_OPPOSITE[f][m]) for m in range(np.shape(MATRIX_OPPOSITE)[1]) ]) ))
+			nextf = nextf+args.fbin
+		writer.close()
