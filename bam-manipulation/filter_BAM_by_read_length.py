df84248a64234b3cb62bb9fc9ad518ed06ab9ad0
owlang <oliviawlang@gmail.com>
2024-09-11 15:51:00 -0400

    reword usage and docstrings

--- bam-manipulation/filter_BAM_by_read_length.py
+++ bam-manipulation/filter_BAM_by_read_length.py
@@ -2,8 +2,6 @@ import sys, getopt
 import pysam
 import argparse
 
-ALL_NT = ["A","T","C","G"]
-
 def getParams():
 	'''Parse parameters from the command line'''
 	parser = argparse.ArgumentParser(description='''
@@ -11,16 +9,16 @@ This script will filter a BAM file by mapped read length (Nanopore or Read1 filt
 
 Example: python filter_BAM_by_read_length.py -i INPUT.bam -o OUTPUT.bam -x 1000 -n 200''')
 	parser.add_argument('-i','--input', metavar='input', required=True, help='a BAM file to filter')
-	parser.add_argument('-o','--output', metavar='cdt_fn', required=True, help='the BAM file filtered by read lengths')
+
+	parser.add_argument('-o','--output', metavar='output', required=True, help='the BAM file filtered by read lengths')
 	parser.add_argument('-x','--max', metavar='max_val', type=int, default=-1, help='max read length to keep')
 	parser.add_argument('-n','--min', metavar='min_val', type=int, default=-1, help='min read length to keep')
-
-	#parser.add_argument('-t','--title', metavar='figure_title', dest='title', required=True, help='')
 
 	args = parser.parse_args()
 	return(args)
 
 def validateBAM(bam):
+	'''Validate BAM file'''
 	try:
 		samfile = pysam.AlignmentFile(bam, "rb")
 		index = samfile.check_index()
@@ -36,7 +34,7 @@ def validateBAM(bam):
 		return True
 
 if __name__ == "__main__":
-	'''Write BED file in new order.'''
+	'''Write BAM file that meets the filter criteria.'''
 	args = getParams()
 
 	# Validate BAM file
